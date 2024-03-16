068bab54765f649dcc1a76355a70eae49cc8361d
[Krushnal121] <krushnalpatil1111@gmail.com>
2024-03-16 14:42:58 +0530

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,11 @@
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
 
-
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
